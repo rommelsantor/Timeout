67b9c14aa23cedabfefbd3ed9f1b9225a47785f0
Rommel Santor <rommel@rommelsantor.com>
2020-04-29 20:48:18 -0700

    transpile to es5

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const timeout_1 = require("./lib/timeout");
+var timeout_1 = require("./lib/timeout");
 exports.default = timeout_1.Timeout;
 //# sourceMappingURL=index.js.map
